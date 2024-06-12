19f9e595c789ebb754c62fb264113a566704d00d
ecalasans <ericcalasans@gmail.com>
2024-06-12 12:28:49 -0300

    Working well

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const env = require('./.env');
 const {Telegraf} = require('telegraf');
 const dialogflow = require('@google-cloud/dialogflow-cx');
 const uuid = require('uuid');
+const {message} = require('telegraf/filters');
 
 const project_id = env.DF_PROJECT_ID;
 const location = 'global';
@@ -61,7 +62,6 @@ const queryToMacris = async (query, session_id, parameters = {}) => {
 
     // Retorna a resposta
     return responses.queryResult;
-
 }
 
 // Função assíncrona que captura os parâmetros
@@ -89,88 +89,111 @@ const getStartPage = async (flow_path) => {
     return start_page;
 }
 
+// Função assíncrona que captura o flow_id
+const getCurrentFlowId = (response) => {
+    const current_page_path = response.currentPage.name;
+    const flow_id = current_page_path.split('/').slice(-3, -2)[0];
+    return flow_id;
+}
+
+bot.start(
+    async ctx => {
+        ctx.reply('Grupo Pequenos Passos - Setor de Inovação e Tecnologia')
+    }
+)
+
 // Interação do bot
 bot.on(
-    'text',
+    message('text'),
     async ctx => {
-            const user_id = ctx.update.message.from.id.toString();
+        const user_id = ctx.update.message.from.id.toString();
 
-            // Cria o id de sessão
-            let session_id;
-            if (sessions[user_id]){
-                session_id = sessions[user_id].session_id;
-            } else {
-                session_id = uuid.v4();
-                sessions[user_id] = {
-                    session_id,
-                    data: {}
-                };
-            }
+        // Cria o id de sessão
+        let session_id;
+        if (sessions[user_id]){
+            session_id = sessions[user_id].session_id;
+        } else {
+            session_id = uuid.v4();
+            sessions[user_id] = {
+                session_id,
+                data: {}
+            };
+        }
 
-            // Captura o que foi digitado
-            const query = ctx.update.message.text;
+        // Captura o que foi digitado
+        const query = ctx.update.message.text;
 
-            //Captura a página ou seta para inicial
-            const flow_path = `projects/${project_id}/locations/${location}/agents/${agent_id}/flows/00000000-0000-0000-0000-000000000000`;
+        // Captura o flow Id
+        let flow_id = '00000000-0000-0000-0000-000000000000'; // Default flow ID
+        if (sessions[user_id].data.current_flow) {
+            flow_id = sessions[user_id].data.current_flow;
+        }
 
-            //  Captura a sessão atual
-            const session_data = sessions[user_id].data;
-
-            try {
-                const start_page = await getStartPage(flow_path);
-                const current_page = session_data.current_page || start_page;
-
-
-                const required_params = await getRequiredParameters(current_page);
-                console.log('Required parameters: ', typeof(required_params));
-                const parameters = {}
-
-                // Captura os parâmetros
+        //Captura a página ou seta para inicial
+        //const flow_path = `projects/${project_id}/locations/${location}/agents/${agent_id}/flows/00000000-0000-0000-0000-000000000000`;
+        const flow_path = `projects/${project_id}/locations/${location}/agents/${agent_id}/flows/${flow_id}`;
 
 
-                console.log('Required parameters:', required_params); // Logging required parameters
-                console.log('Session:', session_data); // Logging session data
+        //  Captura a sessão atual
+        const session_data = sessions[user_id].data;
 
-                // Manda para o DF e aguarda a resposta
-                const response = await queryToMacris(query, session_id, parameters);
-                //console.log('Dialogflow CX response:', JSON.stringify(response, null, 2));
+        try {
+            const start_page = await getStartPage(flow_path);
+            const current_page = session_data.current_page || start_page;
+
+
+            const required_params = await getRequiredParameters(current_page);
+            console.log('Required parameters: ', typeof(required_params));
+            const parameters = {}
+
+            // Captura os parâmetros
+
+
+            console.log('Required parameters:', required_params); // Logging required parameters
+            console.log('Session:', session_data); // Logging session data
+
+            // Manda para o DF e aguarda a resposta
+            const response = await queryToMacris(query, session_id, parameters);
+            //console.log('Dialogflow CX response:', JSON.stringify(response, null, 2));
 
             // Update session values
-                session_data.current_page = response.currentPage.name;
-                if (response.parameters && response.parameters.fields) {
-                    Object.keys(response.parameters.fields).forEach(key => {
-                        const value = response.parameters.fields[key];
-                        session_data[key] = value.stringValue || value.structValue;
-                    });
-                }
+            session_data.current_page = response.currentPage.name;
+            session_data.current_flow = getCurrentFlowId(response);
 
-                if (response.responseMessages && response.responseMessages.length > 0) {
-                    response.responseMessages.forEach(
-                        (msg) => {
-                            if (msg.text && msg.text.text){
-                                const reply_msg =  msg.text.text[0]
-                                console.log(`Replying with: ${reply_msg}`);
-                                ctx.reply(reply_msg);
-                            } else {
-                                console.log('Received a response message with no text.');
-                            }
-                        }
-                    );
-                    console.log('Current page:', response.currentPage?.displayName);
-
-                } else {
-                    console.log('No response messages found');
-                    ctx.reply('I didn’t understand that. Can you try rephrasing?');
-                }
-
-                // Manda a resposta para o chat
-                //ctx.reply(response.responseMessages[0].text.text[0]);
-
-            } catch (e) {
-                console.error('Dialogflow CX error: ', e.message);
-                ctx.reply('Deu merda...');
+            if (response.parameters && response.parameters.fields) {
+                Object.keys(response.parameters.fields).forEach(key => {
+                    const value = response.parameters.fields[key];
+                    session_data[key] = value.stringValue || value.structValue;
+                });
             }
+
+            if (response.responseMessages && response.responseMessages.length > 0) {
+                response.responseMessages.forEach(
+                    (msg) => {
+                        if (msg.text && msg.text.text){
+                            const reply_msg =  msg.text.text[0]
+                            console.log(`Replying with: ${reply_msg}`);
+                            ctx.reply(reply_msg);
+                        } else {
+                            console.log('Received a response message with no text.');
+                        }
+                    }
+                );
+                console.log('Current page:', response.currentPage?.displayName);
+
+            } else {
+                console.log('No response messages found');
+                ctx.reply('I didn’t understand that. Can you try rephrasing?');
+            }
+
+            // Manda a resposta para o chat
+            //ctx.reply(response.responseMessages[0].text.text[0]);
+
+        } catch (e) {
+            console.error('Dialogflow CX error: ', e.message);
+            ctx.reply('Deu merda...');
         }
+    }
 )
 
 bot.launch();
